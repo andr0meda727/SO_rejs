62befa5bf5bfd5cf123bba297e9491af2263c256
andr0meda727 <kwwk747@gmail.com>
2025-01-06 18:45:29 +0100

    Add semaphore synchronization in harbour captain
    To prevent race conditions and ensure safe interaction
    with shared resources I added semaphore blocking functions.

--- habourCaptain.c
+++ habourCaptain.c
@@ -1,5 +1,4 @@
-#include "captains.h"
-
+#include "utils.h"
 
 void launchHabourCaptain(int shmid, int semid) {
     SharedMemory *sm = (SharedMemory *)shmat(shmid, NULL, 0);
@@ -16,19 +15,23 @@ void launchHabourCaptain(int shmid, int semid) {
         c = getchar();
         if (c == '\n') continue;
 
+        waitSemaphore(semid, SEM_MUTEX);
         if (c == 'w') {
-            kill(sm->shipCaptainPid, SIGUSR1); // Sig 1
-            printf("=== Habour Captain === Early departure signal sent.\n");
+            kill(sm->shipCaptainPid, SIGUSR1); // Send SIGUSR1 for early departure
+            printf("=== Habour Captain === Early departure signal sent\n");
         } else if (c == 'k') {
-            kill(sm->shipCaptainPid, SIGUSR2); // Sig 2
+            kill(sm->shipCaptainPid, SIGUSR2); // Send SIGUSR2 for end-of-day
             printf("=== Habour Captain === End-of-day signal sent\n");
         } else if (c == 'q') {
+            signalSemaphore(semid, SEM_MUTEX);
             printf("=== Habour Captain === Exiting\n");
             break;
         } else {
             printf("=== Habour Captain === Unknown command\n");
         }
+        signalSemaphore(semid, SEM_MUTEX); 
 
+        // input validation not strong enough probably
         while (getchar() != '\n'); // Clearing buff
     }
 
