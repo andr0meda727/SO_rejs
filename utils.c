60d94515a0d348016f4c65f922b20b877a8d0967
andr0meda727 <kwwk747@gmail.com>
2025-01-06 17:57:22 +0100

    Add semaphore wait and signal functions
    I added the waitSemaphore and signalSemaphore functions to
    facilitate semaphore synchronization. I also moved the functions from main
    to utils.c for better readability and later modification.

---
+++ utils.c
@@ -0,0 +1,120 @@
+#include "utils.h"
+
+int waitSemaphore(int semID, int number) {
+   struct sembuf operation;
+   operacje[0].sem_num = number;
+   operacje[0].sem_op = -1;
+   operacje[0].sem_flg = 0;
+
+   if (semop(semID, operation, 1) == -1) {
+      perror("Error in waitSemaphore");
+      exit(EXIT_FAILURE);
+   }
+
+}
+
+void signalSemaphore(int semID, int number) {
+   struct sembuf operation;
+   operacje[0].sem_num = number;
+   operacje[0].sem_op = 1;
+   operacje[0].sem_flg = 0;
+
+   if (semop(semID, operation, 1) == -1) {
+        perror("Error in signalSemaphore");
+        exit(EXIT_FAILURE);
+   }
+}
+
+int initializeSemaphores() {
+    key_t semKey = ftok(".", SEM_PROJECT_ID);
+    if (semKey == -1) {
+        perror("ftok for sem");
+        exit(EXIT_FAILURE);
+    }
+
+    int semid = semget(semKey, 2, IPC_CREAT | IPC_EXCL | 0600);
+    if (semid == -1) {
+        perror("semget");
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned short initValues[2];
+    initValues[SEM_MUTEX] = 1;
+    initValues[SEM_BRIDGE] = BRIDGE_CAPACITY;
+
+    if (semctl(semid, 0, SETALL, initValues) == -1) {
+        perror("semctl SETALL");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Semaphores created and initialized.\n");
+    
+    return semid;
+}
+
+void cleanupSemaphores(int semid) {
+    if (semctl(semid, 0, IPC_RMID) == -1) {
+        perror("semctl IPC_RMID");
+    }
+}
+
+int initializeSharedMemory() {
+    key_t memoryKey = ftok(".", SHM_PROJECT_ID);
+    if (memoryKey == -1) {
+        perror("ftok for shm");
+        exit(EXIT_FAILURE);
+    }
+
+    int shmid = shmget(memoryKey, sizeof(SharedMemory), IPC_CREAT | IPC_EXCL | 0600);
+    if (shmid == -1) {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+
+    return shmid;
+}
+
+void cleanupSharedMemory(int shmid) {
+    if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+        perror("shmctl");
+    }
+}
+
+void handleInput() {
+    if (SHIP_CAPACITY <= BRIDGE_CAPACITY) {
+        fprintf(stderr, "The bridge capacity must be smaller than the ship capacity.\n");
+        exit(1);
+    }
+
+    if (BRIDGE_CAPACITY < 1) {
+        fprintf(stderr, "The bridge capacity must be greater than 0.\n");
+        exit(2);
+    }
+
+    if (SHIP_CAPACITY < 1) {
+        fprintf(stderr, "The ship capacity must be greater than 0.\n");
+        exit(3);
+    }
+
+    if (TIME_BETWEEN_TRIPS <= TRIP_DURATION) {
+        fprintf(stderr, "The trip duration must be shorter than the time between trips.\n");
+        exit(4);
+    }
+
+    if (TRIP_DURATION < 1) {
+        fprintf(stderr, "The trip cannot last less than 1.\n");
+        exit(5);
+    }
+
+    if (TIME_BETWEEN_TRIPS < 1) {
+        fprintf(stderr, "The time between trips cannot be less than 1.\n");
+        exit(6);
+    }
+
+    if (NUMBER_OF_TRIPS_PER_DAY < 1) {
+        fprintf(stderr, "The number of trips per day must be greater than 0.\n");
+        exit(7);
+    }
+
+    printf("All parameters have been correctly defined.\n");
+}
---
+++ utils.h
@@ -0,0 +1,50 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+
+#define SHIP_CAPACITY 100
+#define BRIDGE_CAPACITY 10
+#define TIME_BETWEEN_TRIPS 60 // [s]
+#define TRIP_DURATION 30 // [s]
+#define NUMBER_OF_TRIPS_PER_DAY 5
+
+#define SHM_PROJECT_ID 'A'
+#define SEM_PROJECT_ID 'B'
+
+// Semaphore indices in the semaphore array
+#define SEM_MUTEX 0      // Semaphore for the critical section
+#define SEM_BRIDGE 1     // Semaphore controlling the number of people on the bridge
+
+typedef struct {
+    int peopleOnShip;
+    int peopleOnBridge;
+    int currentVoyage;      // Current number of completed voyages
+    int signalEarlyVoyage;  // Signal 1
+    int signalEndOfDay;     // Signal 2
+    int queueDirection;     // 0 = towards ship, 1 = towards land
+
+    pid_t harbourCaptainPid;
+    pid_t shipCaptainPid;
+} SharedMemory;
+
+void handleInput();
+void launchShipCaptain(int shmid, int semid);
+void launchHabourCaptain(int shmid, int semid);
+int initializeSharedMemory();
+int initializeSemaphores();
+void cleanupSemaphores(int semid);
+void cleanupSharedMemory(int shmid);
+int waitSemafor(int semID, int number, int flags);
+void signalSemafor(int semID, int number);
+
+#endif 
+
