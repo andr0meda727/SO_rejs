9328b04613057f4410854ad0dc8e7d78fb72dcd1
andr0meda727 <kwwk747@gmail.com>
2025-01-08 20:48:59 +0100

    Add colors to utils output
    If something went successfully, it is marked with green color.
    Otherwise, it is red.

--- utils.c
+++ utils.c
@@ -1,5 +1,9 @@
 #include "utils.h"
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
 void waitSemaphore(int semID, int number) {
    struct sembuf operation;
    operation.sem_num = number;
@@ -7,7 +11,7 @@ void waitSemaphore(int semID, int number) {
    operation.sem_flg = 0;
 
    if (semop(semID, &operation, 1) == -1) {
-      perror("Error in waitSemaphore");
+      perror(RED "Error in waitSemaphore" RESET);
       exit(EXIT_FAILURE);
    }
 }
@@ -19,7 +23,7 @@ void signalSemaphore(int semID, int number) {
    operation.sem_flg = 0;
 
    if (semop(semID, &operation, 1) == -1) {
-        perror("Error in signalSemaphore");
+        perror(RED "Error in signalSemaphore" RESET);
         exit(EXIT_FAILURE);
    }
 }
@@ -27,13 +31,13 @@ void signalSemaphore(int semID, int number) {
 int initializeSemaphores() {
     key_t semKey = ftok(".", SEM_PROJECT_ID);
     if (semKey == -1) {
-        perror("ftok for sem");
+        perror(RED "ftok for sem" RESET);
         exit(EXIT_FAILURE);
     }
 
     int semid = semget(semKey, 2, IPC_CREAT | IPC_EXCL | 0600);
     if (semid == -1) {
-        perror("semget");
+        perror(RED "semget" RESET);
         exit(EXIT_FAILURE);
     }
 
@@ -42,87 +46,93 @@ int initializeSemaphores() {
     initValues[SEM_BRIDGE] = BRIDGE_CAPACITY;
 
     if (semctl(semid, 0, SETALL, initValues) == -1) {
-        perror("semctl SETALL");
+        perror(RED "semctl SETALL" RESET);
         exit(EXIT_FAILURE);
     }
 
-    printf("Semaphores created and initialized.\n");
+    printf(GREEN "Semaphores created and initialized." RESET "\n");
     
     return semid;
 }
 
 void cleanupSemaphores(int semid) {
     if (semctl(semid, 0, IPC_RMID) == -1) {
-        perror("semctl IPC_RMID");
+        perror(RED "semctl IPC_RMID" RESET);
+    } else {
+        printf(GREEN "Semaphores cleaned up successfully." RESET "\n");
     }
 }
 
 int initializeSharedMemory() {
     key_t memoryKey = ftok(".", SHM_PROJECT_ID);
     if (memoryKey == -1) {
-        perror("ftok for shm");
+        perror(RED "ftok for shm" RESET);
         exit(EXIT_FAILURE);
     }
 
     int shmid = shmget(memoryKey, sizeof(SharedMemory), IPC_CREAT | IPC_EXCL | 0600);
     if (shmid == -1) {
-        perror("shmget");
+        perror(RED "shmget" RESET);
         exit(EXIT_FAILURE);
     }
 
+    printf(GREEN "Shared memory segment created successfully." RESET "\n");
     return shmid;
 }
 
 void cleanupSharedMemory(int shmid) {
     if (shmctl(shmid, IPC_RMID, NULL) == -1) {
-        perror("shmctl");
+        perror(RED "shmctl" RESET);
+    } else {
+        printf(GREEN "Shared memory segment cleaned up successfully." RESET "\n");
     }
 }
 
 void handleInput() {
     if (SHIP_CAPACITY <= BRIDGE_CAPACITY) {
-        fprintf(stderr, "The bridge capacity must be smaller than the ship capacity.\n");
+        fprintf(stderr, RED "The bridge capacity must be smaller than the ship capacity." RESET "\n");
         exit(1);
     }
 
     if (BRIDGE_CAPACITY < 1) {
-        fprintf(stderr, "The bridge capacity must be greater than 0.\n");
+        fprintf(stderr, RED "The bridge capacity must be greater than 0." RESET "\n");
         exit(2);
     }
 
     if (SHIP_CAPACITY < 1) {
-        fprintf(stderr, "The ship capacity must be greater than 0.\n");
+        fprintf(stderr, RED "The ship capacity must be greater than 0." RESET "\n");
         exit(3);
     }
 
     if (TIME_BETWEEN_TRIPS <= TRIP_DURATION) {
-        fprintf(stderr, "The trip duration must be shorter than the time between trips.\n");
+        fprintf(stderr, RED "The trip duration must be shorter than the time between trips." RESET "\n");
         exit(4);
     }
 
     if (TRIP_DURATION < 1) {
-        fprintf(stderr, "The trip cannot last less than 1.\n");
+        fprintf(stderr, RED "The trip cannot last less than 1." RESET "\n");
         exit(5);
     }
 
     if (TIME_BETWEEN_TRIPS < 1) {
-        fprintf(stderr, "The time between trips cannot be less than 1.\n");
+        fprintf(stderr, RED "The time between trips cannot be less than 1." RESET "\n");
         exit(6);
     }
 
     if (NUMBER_OF_TRIPS_PER_DAY < 1) {
-        fprintf(stderr, "The number of trips per day must be greater than 0.\n");
+        fprintf(stderr, RED "The number of trips per day must be greater than 0." RESET "\n");
         exit(7);
     }
 
-    printf("All parameters have been correctly defined.\n");
+    printf(GREEN "All parameters have been correctly defined." RESET "\n");
 }
 
 SharedMemory* attachSharedMemory(int shmid) {
     SharedMemory* sm = (SharedMemory *)shmat(shmid, NULL, 0);
     if (sm == (void *)-1) {
-        perror("shmat");
+        perror(RED "shmat" RESET);
         exit(EXIT_FAILURE);
     }
+    printf(GREEN "Shared memory attached successfully." RESET "\n");
     return sm;
 }
